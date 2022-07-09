476ca7ac5976720dbe13f2ec67ab96678a2b5d01
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2022-07-08 21:38:40 -0700

    Add light lookup funcs using hashicorp/mdns

---
+++ elgato/lookup.go
@@ -0,0 +1,49 @@
+package elgato
+
+import (
+	"net"
+	"strings"
+	"sync"
+
+	"github.com/hashicorp/mdns"
+)
+
+const (
+	MDNSService   = "_elg._tcp"
+	NameRingLight = "Elgato Ring Light"
+)
+
+type Device struct {
+	Name string
+	IP   net.IP
+	Port uint16
+}
+
+func FindDevices(searchStr string) ([]Device, error) {
+	var devices []Device
+	entriesCh := make(chan *mdns.ServiceEntry, 4)
+
+	// Using a WaitGroup so we can make sure our goroutine finishes processing before the return happens.
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for entry := range entriesCh {
+			if strings.Contains(entry.Info, searchStr) {
+				devices = append(devices, Device{
+					Name: entry.Host,
+					IP:   entry.AddrV4,
+					Port: uint16(entry.Port),
+				})
+			}
+		}
+	}()
+
+	err := mdns.Lookup(MDNSService, entriesCh)
+	close(entriesCh)
+
+	// Wait until the goroutine finishes writing to `devices`.
+	wg.Wait()
+	return devices, err
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/seanpfeifer/elgato-light-control
 
 go 1.18
 
-require github.com/seanpfeifer/rigging v0.2.0
+require (
+	github.com/hashicorp/mdns v1.0.5
+	github.com/seanpfeifer/rigging v0.2.0
+)
+
+require (
+	github.com/miekg/dns v1.1.41 // indirect
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
+	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+)
