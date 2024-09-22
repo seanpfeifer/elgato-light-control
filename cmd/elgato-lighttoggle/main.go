4f8f8023e3125cd440893155ec73ed92012bfa90
Sean Pfeifer <seanpfeifer@users.noreply.github.com>
2024-09-22 01:11:07 -0700

    Modify lookup to accept an interface to work in Win11

--- cmd/elgato-lighttoggle/main.go
+++ cmd/elgato-lighttoggle/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"log"
+	"net"
 
 	"github.com/seanpfeifer/elgato-light-control/elgato"
 	"github.com/seanpfeifer/rigging/logging"
 )
 
 func main() {
-	devices, err := elgato.FindDevices(elgato.NameRingLight)
+	ifaces, err := net.Interfaces()
+	logging.FatalIfError(err, "getting interfaces")
+	// I'm just going to assume the interface I need is the first one
+	devices, err := elgato.FindDevices(elgato.NameRingLight, ifaces[0])
 	logging.FatalIfError(err, "finding lights")
 	if len(devices) < 1 {
 		return
 	}
+
+	// https://github.com/hashicorp/mdns/issues/80
 
 	address := devices[0].IP.String()
 	port := devices[0].Port
